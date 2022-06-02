093968751fdd7a0a795e8dc03f0c6d7d4e38b6ab
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2022-06-02 20:44:42 +0000

    Add grid graph utilities and maze traversal algorithms

---
+++ Assets/Scripts/MazeGeneration/GridTraversal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace ShareefSoftware
+{
+    /// Utility class to traverse a grid.
+    public class GridTraversal<T>
+    {
+        private bool[,] visited;
+        private readonly IGridGraph<T> grid;
+
+        /// Constructor
+        public GridTraversal(IGridGraph<T> grid)
+        {
+            this.grid = grid;
+        }
+        /// Depth First Search Enumeration
+        public IEnumerable<((int Row, int Column) From, (int Row, int Column) To)> DepthFirst(int startRow, int startColumn)
+        {
+            visited = new bool[grid.NumberOfRows, grid.NumberOfColumns];
+            return TraverseComponentRecursively((startRow, startColumn));
+        }
+
+        private IEnumerable<((int Row, int Column) From, (int Row, int Column) To)> TraverseComponentRecursively((int Row, int Column) cell)
+        {
+            if (!visited[cell.Row, cell.Column])
+            {
+                visited[cell.Row, cell.Column] = true;
+                foreach (var neighbor in grid.Neighbors(cell.Row, cell.Column))
+                {
+                    if (!visited[neighbor.Row,neighbor.Column])
+                    {
+                        foreach (var edge in TraverseComponentRecursively(neighbor))
+                            yield return edge;
+                        yield return ((cell.Row, cell.Column),neighbor);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MazeGeneration/IGridGraph.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ShareefSoftware
+{
+    public interface IGridGraph<T> : IEnumerable<(int Row, int Column, T NodeValue)>
+    {
+        int NumberOfRows { get; }
+        int NumberOfColumns { get; }
+
+        T GetCellValue(int row, int column);
+        IEnumerable<(int Row, int Column)> Neighbors(int row, int column);
+    }
+}
